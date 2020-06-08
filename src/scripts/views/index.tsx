0a9a279627ca03d0d86d98202ec30d265fe844bc
Balaganesh Damodaran <asleepysamurai@gmail.com>
2020-06-08 21:29:33 +0200

    Implemented Better styling for signup page

--- src/scripts/views/index.tsx
+++ src/scripts/views/index.tsx
@@ -4,10 +4,12 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import 'styles/bootstrap.scss';
+import 'styles/custom.scss';
+import 'styles/flex.scss';
+import 'styles/app.scss';
 
 export const AppRoot: React.FunctionComponent = ({ children }) => {
-  return <div className="root-container flex-container">{children}</div>;
+  return <div className="root-container flex-container column h-100 w-100">{children}</div>;
 };
 
 AppRoot.propTypes = {
--- src/scripts/views/user/signup.tsx
+++ src/scripts/views/user/signup.tsx
@@ -3,20 +3,28 @@
  */
 
 import React from 'react';
-import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { Form, FormGroup, Input, Button } from 'reactstrap';
 
 export const Signup: React.FunctionComponent = () => {
   return (
-    <div className="container">
-      <Form className="offset-md-4 col-md-4">
+    <div className="container flex-container column center text-center">
+      <Form className="offset-md-4 col-md-4 py-5">
         <FormGroup>
-          <Label for="email">Email</Label>
-          <Input />
+          <Input placeholder="Enter your email" />
         </FormGroup>
         <FormGroup>
           <Button color="primary">Signup Now!</Button>
         </FormGroup>
       </Form>
+      <p>
+        <em>
+          <small>
+            <a href="/user/login" className="text-secondary">
+              Already have an F-App account? Click here to login to your account instead.
+            </a>
+          </small>
+        </em>
+      </p>
     </div>
   );
 };
